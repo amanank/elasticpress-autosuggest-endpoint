d48775ddf2681d62a6bd66cee66bae3295e658dc
Atta Khalid <amanank@yahoo.com>
2020-12-22 22:54:23 +0000

    Use elasticsearch suggest query for term suggestions

--- elasticpress-autosuggest-endpoint.php
+++ elasticpress-autosuggest-endpoint.php
@@ -9,12 +9,16 @@
  License URI:  https://www.gnu.org/licenses/gpl-2.0.html
  */
 
+
 /** 
  * Hook to override elasticpress exposing the entire elastic search query to the client.
- * Only the search text is required from teh client
+ * Only the search text is required from the client
  */
 function endpoint_ep_autosuggest_options($options) {
     $options['query'] = '{"q":"'.$options['placeholder'].'"}';
+    $options['highlightingEnabled'] = false;
+    $options['action'] = 'select';
+
     return $options;
 }
 add_filter('ep_autosuggest_options', 'endpoint_ep_autosuggest_options', 10, 1);
@@ -38,9 +42,7 @@ add_action( 'rest_api_init', function() {
 /**
  * Elasticpress Autosuggest Endpoint Callback
  *
- * gets host and index name dynamically. Otherwise,
- * if not specified, host would default to localhost:9200
- * and index name would default to 'index'
+ * gets host and index name from ElasticPress
  *
  * @param \WP_REST_Request $data
  * @return array|callable
@@ -50,12 +52,26 @@ function ep_autosuggest( \WP_REST_Request $data ) {
     //replace double quotes with spaces
     $q = str_replace('"', ' ', $data->get_param('q')); 
 
-    //get the search query from elasticpress the one we removed fro client
-    $features = ElasticPress\Features::factory();
-    $qry = $features->get_registered_feature('autosuggest')->generate_search_query();
-
-    //replace the search text with the query placeholder
-    $qry = str_replace($qry['placeholder'], $q, $qry['body']);
+    //Elastic query to get suggestion from post_content
+    $qry = json_encode(array(
+        'suggest'=>array(
+            'text' => $q,
+            'simple_phrase' => array(
+                'phrase'=>array(
+                    'field' => 'post_content.term_suggest',
+                    'size' => 5,
+                    'gram_size' => 3,
+                    'direct_generator' => array(array(
+                        'field' => 'post_content.term_suggest',
+                        'suggest_mode' => 'popular'
+                    )),
+                    'highlight' => array(
+                        'pre_tag' => "<mark class=ep-highlight>", //no quotes around class because ElasticPress Autosuggest js escapes double quotes :(
+                        'post_tag' => '</mark>'
+                    )
+                )
+            )
+        )));
 
     //get elasticsearch host and index name from elasticpress
     $host = ElasticPress\Utils\get_host();
@@ -64,5 +80,14 @@ function ep_autosuggest( \WP_REST_Request $data ) {
     //run the elastic query
     $request = wp_remote_request( "{$host}$index/_search", array('method'=>'POST', 'body'=>$qry, 'headers'=>array('Content-Type' => 'application/json'))); 
 
-    return json_decode(wp_remote_retrieve_body( $request ));
+    $resp = json_decode(wp_remote_retrieve_body( $request ));
+
+    //format results
+    $resp->hits->hits = [];
+    foreach ($resp->suggest->simple_phrase[0]->options as $hit) {
+        $hit->_source = array('post_title'=>$hit->highlighted, 'permalink'=>'#');
+        $resp->hits->hits[] = $hit;
+    };
+
+    return $resp;
 }
